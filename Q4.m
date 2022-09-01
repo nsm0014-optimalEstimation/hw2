432a0257e3074e498e460519f790263ba25a93da
Noah Miller <nsm0014@auburn.edu>
2022-09-01 11:24:47 -0500

    started Q4 partb

--- Q4.m
+++ Q4.m
@@ -13,4 +13,12 @@ y = dlsim(numd, dend, u);
 sigma = 0.01;
 Y = y + sigma * randn(1000, 1);
 
-H = [u(2:end-1) -u(2:end-2) Y(2:end-1) -Y(2:end-2)];
+H = [u(2:end-1) -u(1:end-2) Y(2:end-1) -Y(1:end-2)];
+est = (H' * H)^-1 * H' * Y(3:end)
+
+numesti = [est(1) -est(2)];
+denesti = [1 -est(3) est(4)];
+figure
+hold on
+bode(tf(numd,dend,1))
+bode(tf(numesti,denesti,1))
